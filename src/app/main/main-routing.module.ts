adc6bd62e003310f00c475f1739172f444c60f90
Chidimma Nwanya <chidynwanya11@gmail.com>
2020-07-17 14:24:13 +0100

    added the congratulations, product view and product checkout components

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,13 +1,17 @@
+import { ProductCheckoutComponent } from './product-checkout/product-checkout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { LandingComponent } from './landing/landing.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent, 
   children: [
-    { path: '', component: LandingComponent }
+    { path: '', component: LandingComponent },
+    { path: 'product-view', component: ProductViewComponent },
+    { path: 'product-checkout', component: ProductCheckoutComponent }
   ]
   }
 ];
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -6,10 +6,16 @@ import { MainComponent } from './main.component';
 import { LandingComponent } from './landing/landing.component';
 import { AuthenticationModule } from '../authentication/authentication.module';
 import { SharedModule } from '../shared/shared.module';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductCheckoutComponent } from './product-checkout/product-checkout.component';
+import { CheckoutPageComponent } from './product-checkout/checkout-page/checkout-page.component';
+import { PaymentComponent } from './product-checkout/payment/payment.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CongratulationsComponent } from './congratulations/congratulations.component';
 
 
 @NgModule({
-  declarations: [MainComponent, LandingComponent],
+  declarations: [MainComponent, LandingComponent, ProductViewComponent, ProductCheckoutComponent, CheckoutPageComponent, PaymentComponent, OrdersComponent, CongratulationsComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
