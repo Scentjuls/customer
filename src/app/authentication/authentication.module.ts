332d1b40eb59dfd6c586d59ecfc24664bfb269f0
Chidimma Nwanya <chidynwanya11@gmail.com>
2020-07-17 12:33:53 +0100

    added modals

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateAccountComponent, SignInComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
     FormsModule,
     AuthenticationRoutingModule
-  ]
+  ],
+  exports: [CreateAccountComponent, SignInComponent, ForgotPasswordComponent]
 })
 export class AuthenticationModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { LandingComponent } from './landing/landing.component';
+import { AuthenticationModule } from '../authentication/authentication.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [MainComponent, LandingComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    SharedModule,
+    AuthenticationModule
   ]
 })
 export class MainModule { }
